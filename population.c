f65839d58318abd6bc745837c87c89374e86ce79
juliakisielewicz <juliakisielewicz@users.noreply.github.com>
2021-07-13 10:53:40 +0000

    automated commit by submit50

--- population.c
+++ population.c
@@ -10,26 +10,30 @@ int main(void)
 
     // TODO: Prompt for start size
     int start = 0;
-    do{
+    do
+    {
         start = get_int("Enter starting size (min 9): ");
-    }while(start < 9);
+    }
+    while (start < 9);
     
 
 
     // TODO: Prompt for end size
     int end = 0;
-    do{
+    do
+    {
         end = get_int("Enter ending size: ");
-    }while(end < start);
+    }
+    while (end < start);
 
 
 
     // TODO: Calculate number of years until we reach threshold
     int population = start;
     int years = 0;
-    while(population < end)
+    while (population < end)
     {
-        population = population + (population/3) - (population/4);
+        population = population + (population / 3) - (population / 4);
         years++;
     }
 
