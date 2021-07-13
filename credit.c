e2c92171e0b60c9c7cb4b281caca69f1c1096f01
juliakisielewicz <juliakisielewicz@users.noreply.github.com>
2021-07-13 14:14:26 +0000

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -16,15 +16,21 @@ int main(void)
     //Calculating sums and number of digits 
     while (n != 0)
     {
-        if(count % 2 == 0)
+        if (count % 2 == 0)
+        {
             sum1 = sum1 + (n % 10);
+        }
         else
         {
             tmp = 2 * (n % 10);
-            if ( tmp > 9 )
+            if (tmp > 9)
+            {
                 sum2 = sum2 + (tmp % 10) + (tmp / 10);
+            }
             else
+            {
                 sum2 = sum2 + tmp;
+            }
         }
         
         n = n / 10;
@@ -35,17 +41,28 @@ int main(void)
     int checksum = sum1 + sum2;
 
     //Displaying proper information
-    if(checksum % 10 == 0)
+    if (checksum % 10 == 0)
     {
-        if (count == 15 && ((number / (long) pow(10,13)) == 34 ||  (number / (long) pow(10,13)) == 37)) 
+        if (count == 15 && ((number / (long) pow(10, 13)) == 34 || (number / (long) pow(10, 13)) == 37)) 
+        {
             printf("AMEX\n");
-        else if (count == 16 && (number / (long) pow(10,14) == 51 || number / (long) pow(10,14) == 52 ||number / (long) pow(10,14) == 53 ||number / (long) pow(10,14) == 54 ||number / (long) pow(10,14) == 55)) 
+        }
+        else if (count == 16 && (number / (long) pow(10, 14) == 51 || number / (long) pow(10, 14) == 52
+                                 || number / (long) pow(10, 14) == 53 || number / (long) pow(10, 14) == 54 || number / (long) pow(10, 14) == 55)) 
+        {
             printf("MASTERCARD\n");
-        else if ((count == 13 && number / (long) pow(10,12) == 4) || (count == 16 && number / (long) pow(10,15) == 4))
+        }
+        else if ((count == 13 && number / (long) pow(10, 12) == 4) || (count == 16 && number / (long) pow(10, 15) == 4))
+        {
             printf("VISA\n");
+        }
         else
+        {
             printf("INVALID\n");
+        }
     }
     else
+    {
         printf("INVALID\n");
+    }
 }
