c755853cfa1ef14ef3ed6c73d3905408ee5fed53
juliakisielewicz <juliakisielewicz@users.noreply.github.com>
2021-07-13 13:45:01 +0000

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -38,9 +38,9 @@ int main(void)
     {
         if (count == 15 && ((number / (long) pow(10,13)) == 34 ||  (number / (long) pow(10,13)) == 37)) 
             printf("AMEX\n");
-        else if(count == 16 && (number / (long) pow(10,14) == 51 || number / (long) pow(10,14) == 52 ||number / (long) pow(10,14) == 53 ||number / (long) pow(10,14) == 54 ||number / (long) pow(10,14) == 55)) 
+        else if (count == 16 && (number / (long) pow(10,14) == 51 || number / (long) pow(10,14) == 52 ||number / (long) pow(10,14) == 53 ||number / (long) pow(10,14) == 54 ||number / (long) pow(10,14) == 55)) 
             printf("MASTERCARD\n");
-        if ((count == 13 && number / (long) pow(10,12) == 4) || (count == 16 && number / (long) pow(10,15) == 4))
+        else if ((count == 13 && number / (long) pow(10,12) == 4) || (count == 16 && number / (long) pow(10,15) == 4))
             printf("VISA\n");
         else
             printf("INVALID\n");
