f45451b507c8fa1b3617244a823a3d548878ef04
juliakisielewicz <juliakisielewicz@users.noreply.github.com>
2021-07-13 13:50:07 +0000

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -2,17 +2,18 @@
 #include <math.h>
 #include <stdio.h>
 
-
-
 int main(void)
 {
-    
+    //Getting number
     long number = get_long("Number: ");
     
+    //Preparing variables
     long n = number;
     int sum1 = 0, sum2 = 0;
     int count = 0;
-    int tmp;
+    int tmp = 0;
+    
+    //Calculating sums and number of digits 
     while (n != 0)
     {
         if(count % 2 == 0)
@@ -30,10 +31,10 @@ int main(void)
         count++;
     }
     
+    //Final checksum
     int checksum = sum1 + sum2;
-    printf("checksum: %i\n", checksum);
-    printf("number od digits: %i\n", count);
 
+    //Displaying proper information
     if(checksum % 10 == 0)
     {
         if (count == 15 && ((number / (long) pow(10,13)) == 34 ||  (number / (long) pow(10,13)) == 37)) 
