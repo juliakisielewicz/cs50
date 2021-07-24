4521c3065f7fe4b9014ae48b5e917e36392be1f6
juliakisielewicz <juliakisielewicz@users.noreply.github.com>
2021-07-24 11:45:26 +0000

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -4,32 +4,18 @@
 #include <string.h>
 #include <math.h>
 
-
-int countLetters(string txt);
-int countWords(string txt);
-int countSentences(string txt);
-
 int calculateIndex(string txt);
 
 int main(void)
 {
-    
     //Get input
     string text = get_string("Text: ");
     
-    //int letters = countLetters(text);
-    //int words = countWords(text);
-    //int sentences = countSentences(text);
-    
     // Calculate result
     int grade;
-    //grade = round(0.0588 * (letters * 100 / words) - 0.296 * (sentences * 100 / words) - 15.8);
-
-
     grade = calculateIndex(text);
     
     //Display result
-    
     if (grade < 1)
     {
         printf("Before Grade 1\n");
@@ -45,53 +31,7 @@ int main(void)
     
 }
 
-//Functions
-
-int countLetters(string txt)
-{
-    int sum = 0;
-    
-    for (int i = 0; i < strlen(txt); i++)
-    {
-        if (isalpha(txt[i]))
-        {
-            sum++;
-        }
-    }
-    
-    return sum;
-}
-
-int countWords(string txt)
-{
-    int sum = 0;
-    
-    for (int i = 0; i < strlen(txt); i++)
-    {
-        if (isspace(txt[i]) || txt[i] == '\'')
-        {
-            sum++;
-        }
-    }
-    
-    return sum + 1;  
-}
-
-int countSentences(string txt)
-{
-    int sum = 0;
-    
-    for (int i = 0; i < strlen(txt); i++)
-    {
-        if (txt[i] == '.' || txt[i] == '!' || txt[i] == '?')
-        {
-            sum++;
-        }
-    }
-    
-    return sum;
-}
-
+//Function
 
 int calculateIndex(string txt)
 {
