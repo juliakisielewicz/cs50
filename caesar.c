1b4a88ab6420ad3316b7fd2991a199191025e867
juliakisielewicz <juliakisielewicz@users.noreply.github.com>
2021-07-26 21:39:22 +0000

    automated commit by submit50

--- caesar.c
+++ caesar.c
@@ -17,7 +17,7 @@ int main(int argc, string argv[])
     
     for (int i = 0; i < strlen(argv[1]); i++)
     {
-        if(! isdigit(argv[1][i]))
+        if (! isdigit(argv[1][i]))
         {
             printf("Usage: ./ceasar key\n");
             return 1;
