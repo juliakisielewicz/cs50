e8b6d27e3cda7b8199944a33a4686ddee29897f7
juliakisielewicz <juliakisielewicz@users.noreply.github.com>
2021-08-31 09:49:44 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
+ 
+ const int BLOCK_SIZE = 512;
  
 int main(int argc, char *argv[])
 {
@@ -15,4 +19,52 @@ int main(int argc, char *argv[])
         printf("Could not open file.\n");
         return 1;
     }
+    
+    uint8_t block[BLOCK_SIZE];
+    int counter = 0;
+    char *filename = "";
+    
+    sprintf(filename, "%03i.jpg", counter);
+    FILE *img = fopen(filename, "w");
+    
+    if (img == NULL)
+    {
+        fclose(file);
+        fprintf(stderr, "Could not create %s.\n", filename);
+        return 1;
+    }
+    
+    while (fread(&block, sizeof(uint8_t), BLOCK_SIZE, file) == 512)
+    {
+        if(block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff && (block[3] & 0xf0) == 0xe0)
+        {
+            if (counter != 0)
+            {
+                fclose(img);
+                sprintf(filename, "%03i.jpg", counter);
+                img = fopen(filename, "w");
+                
+                if (img == NULL)
+                {
+                    fclose(file);
+                    fprintf(stderr, "Could not create %s.\n", filename);
+                    return 1;
+                }
+            }
+            
+            fwrite(&block, sizeof(uint8_t), BLOCK_SIZE, img);
+            
+            counter++;
+        }
+        else
+        {
+            if (counter != 0)
+            {
+                fwrite(&block, sizeof(uint8_t), BLOCK_SIZE, img);
+            }
+        }
+    }
+    
+    fclose(file);
+    fclose(img);
 }
