93fb70f0b020885df2ff50257bd3c0c5da8bbce5
juliakisielewicz <juliakisielewicz@users.noreply.github.com>
2021-09-28 09:41:54 +0000

    automated commit by check50 [check50=True]

--- mario.py
+++ mario.py
@@ -3,7 +3,7 @@ from cs50 import get_int
 
 while(True):
     height = get_int("Height: ")
-    if height > 1 and height < 9:
+    if height > 0 and height < 9:
         break
     
 for i in range(1, height+1):
